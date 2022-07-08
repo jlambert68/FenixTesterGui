7d82f8a39b3cee5c1953dae810c097309fc90950
jlamber <jonas.lambert@drakon.se>
2022-07-08 15:04:42 +0200

    Deletion execution was implemented

--- commandAndRuleEngine/ruleEngine_delete.go
+++ commandAndRuleEngine/ruleEngine_delete.go
@@ -1,5 +1,7 @@
 package commandAndRuleEngine
 
+import "errors"
+
 // Verify if an element can be deleted or not, regarding deletion rules
 func (commandAndRuleEngineObject *commandAndRuleEngineObjectStruct) verifyIfElementCanBeDeleted(elementUuid string) (canBeDeleted bool, matcheSimpldRule string, matchedComplexRule string, err error) {
 
@@ -16,4 +18,28 @@ func (commandAndRuleEngineObject *commandAndRuleEngineObjectStruct) verifyIfElem
 	matchedComplexRule, err = commandAndRuleEngineObject.verifyIfComponentCanBeDeletedWithComplexRules(elementUuid)
 
 	return canBeDeleted, matcheSimpldRule, matchedComplexRule, err
+}
+
+// Delete an element, but first ensure that rules for deletion are used
+func (commandAndRuleEngineObject *commandAndRuleEngineObjectStruct) executeDeleteElement(elementUuid string) (err error) {
+
+	// Verify that element is allowed, and can be deleted
+	canBeDeleted, matcheSimpldRule, _, err := commandAndRuleEngineObject.verifyIfElementCanBeDeleted(elementUuid)
+
+	// If there was an error from delete verification then exit
+	if err != nil {
+		return err
+	}
+
+	// If the component couldn't be deleted then exit with error message
+	if canBeDeleted == false {
+		err = errors.New("Element couldn't be deleted due to deletion rule '" + matcheSimpldRule + "'")
+
+		return err
+	}
+
+	// Execute deletion of element
+	err = commandAndRuleEngineObject.executeDeleteElement(elementUuid)
+
+	return err
 }
